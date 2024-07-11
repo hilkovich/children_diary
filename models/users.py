bf20c510c4b5272cb42019b3869f53a2620f4c26
nikolai <n.hilkovich@yandex.ru>
2024-07-11 15:25:13 +0300

    Добавление модели пользователя

---
+++ models/users.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, DateTime, MetaData
+from sqlalchemy.orm import DeclarativeBase
+
+from database.connection import Engine
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    telegram_id = Column(Integer, unique=True)
+    created_on = Column(DateTime)
+
+
+if __name__ == "__main__":
+    metadata = MetaData()
+    Base.metadata.create_all(bind=Engine)
