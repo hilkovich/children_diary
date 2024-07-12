4ac61f16674a0bf90ce7f3f5247a0559869b6930
nikolai <n.hilkovich@yandex.ru>
2024-07-12 09:48:55 +0300

    Добавление модели книги

--- models/books.py
+++ models/books.py
@@ -3,25 +3,20 @@ from sqlalchemy.orm import DeclarativeBase, relationship
 
 from database.connection import Engine
 from models.users import User
-from models.books import Book
 
 
 class Base(DeclarativeBase):
     pass
 
 
-class History(Base):
-    __tablename__ = "history"
+class Book(Base):
+    __tablename__ = "books"
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey(User.id))
     user = relationship(User)
-    book_id = Column(Integer, ForeignKey(Book.id))
-    book = relationship(Book)
-    data_task = Column(DateTime)
-    photo_caption = Column(String)
-    photo_descript = Column(String)
-    photo_story = Column(String)
-    story_save = Column(Integer)
+    data_book = Column(DateTime)
+    book_num = Column(Integer)
+    book_name = Column(String)
 
 
 if __name__ == "__main__":
