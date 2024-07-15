e41fb68702a7e521ce7c2056865fa0aa931a3111
nikolai <n.hilkovich@yandex.ru>
2024-07-15 19:25:32 +0300

    Добавление тестирования history

---
+++ tests/test_history.py
@@ -0,0 +1,18 @@
+import pytest
+
+from repository.history import add_history, get_all_history
+
+
+def test_add_history():
+    telegram_id = 123058345
+    photo_caption = "Тестовое описание фотографий"
+    photo_descript = "Тестовое примечание"
+    photo_story = "Тестовая история"
+    book_id = 12
+    add_history(telegram_id, photo_caption, photo_descript, photo_story, book_id, 1)
+    all_history = get_all_history(telegram_id, book_id)
+    assert len(all_history) == 1
+
+
+if __name__ == "__main__":
+    pytest.main()
