4e38e916a3af6679bebec849980f581a0f12661f
nikolai <n.hilkovich@yandex.ru>
2024-07-15 19:16:09 +0300

    Добавление тестирования books

---
+++ tests/test_books.py
@@ -0,0 +1,16 @@
+import pytest
+
+from repository.books import get_num_book, add_book, get_name_book
+
+
+def test_add_book():
+    add_book(123058345, "Тестовая книга №1")
+    assert get_num_book(123058345) is not None
+
+
+def test_name_book():
+    assert get_name_book(123058345, 1).book_name == "Тестовая книга №1"
+
+
+if __name__ == "__main__":
+    pytest.main()
