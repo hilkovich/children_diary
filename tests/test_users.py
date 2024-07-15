ed5e6afb7fcd3012d2529df72fa2b1153e1c7844
nikolai <n.hilkovich@yandex.ru>
2024-07-15 17:47:28 +0300

    Добавление тестирования users

---
+++ tests/test_users.py
@@ -0,0 +1,13 @@
+import pytest
+
+from repository.users import add_user, get_user
+
+
+def test_add_user():
+    add_user(123058345)
+    user = get_user(123058345)
+    assert user.telegram_id == 123058345
+
+
+if __name__ == "__main__":
+    pytest.main()
