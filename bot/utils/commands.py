22cae7edd2681aef9e830c754d9b510b68f2126d
nikolai <n.hilkovich@yandex.ru>
2024-07-21 19:40:37 +0300

    Добавление команды help

--- bot/utils/commands.py
+++ bot/utils/commands.py
@@ -5,6 +5,7 @@ from aiogram.types import BotCommand, BotCommandScopeDefault
 async def set_commands(bot: Bot):
     commands = [
         BotCommand(command="start", description="Запустить ботика"),
+        BotCommand(command="help", description="Как пользоваться ботиком"),
         BotCommand(command="new", description="Создать новую историю"),
         BotCommand(command="books", description="Показать все книги"),
     ]
